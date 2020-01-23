27c19f88f65e81e17e459317d63ff9173e1162c3
mageeli <mousaageeli@gmail.com>
2020-01-23 11:32:45 +0300

    add database

--- geoDjango/settings.py
+++ geoDjango/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'mydb',
         'USER': 'postgres',
-        'PASSWORD': 'YOUR_DATABASE_USER_PASSWORD',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
